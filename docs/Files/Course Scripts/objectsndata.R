95669493d5c3e963212b44779ecd7817e4fe2632
Benedikt Fritz <b.fritz@zeppelin-university.net>
2019-06-11 09:52:18 +0200

    Subchapter Structure for DataManagement

--- docs/Files/Course Scripts/objectsndata.R
+++ docs/Files/Course Scripts/objectsndata.R
@@ -53,7 +53,8 @@ rm(list=ls()) # all objects are removed
 #' 
 #' ***
 #' ## Vectors
-#' For statistical calculations, we obviously need to work with data sets including many numbers of instead of scalars. The simplest way to collect many numbers (or other tpyes of information) is called a vector in R terminology (you have already been familiarized with vectors on the page before). 
+#' For statistical calculations, we obviously need to work with data sets including many numbers of instead of scalars. The simplest way to collect many numbers (or other
+#' types of information) is called a vector in R terminology (you have already been familiarized with vectors on the page before). 
 #' 
 #' To define a vector, we can collect different values using ```c(value1, value2,...)```. 
 ## ------------------------------------------------------------------------
@@ -73,7 +74,7 @@ b <- a + 1
 b
 
 #' 
-#' R is object based coding --> it follows average math rules
+#' R is object-oriented programming --> accordingly, it follows average math rules.
 ## ------------------------------------------------------------------------
 c <- sqrt(a + b * 2)
 c
@@ -83,36 +84,39 @@ c
 #' Important R functions for vectors:
 #' 
 #' <span style="color:red">Task</span> <u>**Do you understand all of these functions?**</u>
-## ------------------------------------------------------------------------
-# Important R functions for vectors:
-# Basic functions:
-sort(a)
-length(a)
-min(a)
-max(a)
-sum(a)
-prod(a)
-
-# Creating special vectors:
-rep(1, 20)
-seq(50)
-5:15
-seq(4, 20, 2)
+## ---- eval=FALSE---------------------------------------------------------
+## # Important R functions for vectors:
+## # Basic functions:
+## sort(a)
+## length(a)
+## min(a)
+## max(a)
+## sum(a)
+## prod(a)
+## 
+## # Creating special vectors:
+## rep(1, 20)
+## seq(50)
+## 5:15
+## seq(4, 20, 2)
 
 #' 
 #' ***
 #' ### Logical Operators and Logical Vectors
 #' 
+#' <span style="color:red">Task</span> <u>**Do you understand all of these operators?**</u>
 ## ----eval=FALSE----------------------------------------------------------
-## x==y # x is equal to y
-## x>y  # x is bigger then y
-## x<=y # x is smaller or equal to y
-## x!=y # x is NOT equal to y
-## !b   # NOT b (i.e. b is FALSE)
-## a|b  # Either a or b is TRUE
-## a6b  # Both a and b are TRUE
+## x <- 10
+## y <- 9
+## 
+## x == y # x is equal to y
+## x > y  # x is bigger then y
+## x  y # HOW DO YOU TYPE: x is smaller or equal to y
+## x != y # x is NOT equal to y
 
-#' The contents of R vectors do not need to be numeric. A simple example of a different type are character vectors. For handling them, the contents simply need to be enclosed in quotation marks:
+#' 
+#' The contents of R vectors do not need to be numeric. A simple example of a different type are *character* vectors. For handling them, the contents simply need to be
+#' enclosed in quotation marks:
 ## ------------------------------------------------------------------------
 cities = c("Friedrichshafen", "Paris", "Tokio", "Tettnang", "Mailand")
 cities
@@ -121,102 +125,161 @@ cities
 #' Another useful type are **logical vectors**. Each element can only take one of two values: "TRUE" or "FALSE". Internally, "FALSE" corresponds to 0 and "TRUE" to 1. 
 #' 
 ## ------------------------------------------------------------------------
-a <- c(7,2,6,9,4,1,3) 
-b <- a<3 | a>=6 
+
+a <- TRUE; b <- FALSE # note the functionality of the semicolon
+
+a | b  # Either a or b is TRUE
+a & b  # Both a and b are TRUE
+
+a <- c(7, 5, 2, 6, 9, 4, 1, 3)
+b <- a < 3 | a >= 6
 b
 
-#' As we have seen in Econometrics, many variables take only a binary outcome, e.g. they are a dummy variable (for example gender)
 #' 
-#' If we want to store qualitative information with more levels we can use so called **factors**. 
+#' **Summary**: Vectors can have three different classes: "numeric", "logical", "character":
+## ------------------------------------------------------------------------
+x <- c(1050, 35, 2.3, 2 ^ 2, -10, sqrt(81)) # numeric vector
+y <- c(TRUE, FALSE, TRUE, FALSE) # logical vector
+z <- c("zero", "four", "nine") # character vector
+
 #' 
+#' ***
+#' ### Naming and Indexing Vectors
+#' The elements of a vector can be named which can increase the readability of the output. Given a vector ```vec``` and a string vector ```namevec``` of the same length, the names are attached to the vector elements using ```names(vec) <- namevec```.
+#' 
+#' If we want to access a single element or a subset form a vector, we can work with **indices**. They are written in square brackets next to the vector name. For example
+#' ````myvector[4]``` returns the 4th element of ```myvector``` and ```myvector[6] <- 8``` changes the 6th element to take the value of 8. If the vector elements have
+#' names, we can also use those as indices like in ```myvector["elementname"]```
+#' 
+#' <span style="color:red">Task</span> <u>**Complete the empty spaces!**</u>
+## ---- eval=FALSE---------------------------------------------------------
+## # Create a vector "kicker.skills":
+## kicker.skills <- c(0.2, 4, 3, 10, 5,-2)
+## 
+## # Create a string vector of names:
+## players <- c("Katja", "Hans", "Theresa", "Sabine", "Kathrin", "Marco")
+## 
+## # Assign names to vector and display vector:
+## names(kicker.skills) <- players
+## kicker.skills
+## 
+## # Indices by number:
+## kicker.skills[_] # GIVE second entry
+## kicker.skills[_:4] # SELECT 2nd to fourth entry
+## 
+## # Indices by name:
+## kicker.skills["Marco"] # WHAT is the level of Katja's kicker skills?
+## 
+## # Logical indices:
+## # SUBSET for an all-star team, only kicker players with skill level above 2
+## kicker.skills[kicker.skills __ _ ]
+## 
+## # Who is the worst kicker player?
+## kicker.skills[_ == min(_)] # DO you understand every part of this line?
+## 
+
+#' 
+#' ***
+#' 
+#' ## Factors
+#' Seemingly similar to vectors but behaving (sometimes) differently: factors. Factors are very useful to work with binary variables. 
+#' 
+#' <span style="color:red">Task</span> <u>**What is the difference between xf1 and xf2?**</u>
 ## ------------------------------------------------------------------------
 # Costumer Ratings
-x <- c(3,2,2,3,1,2,3,2,1,2)
-xf <- factor(x, labels=c("bad","okay","good")) 
-x
-xf
+x <- c(3, 2, 2, 3, 1, 2, 3, 2, 1, 2)
 
-#' ***
-#' ### Naming and Indexing Vectors
-#' The elements of a vector can be named which can increase the readability of the output. Given a vector vec and a string vector namevec of the same length, the names are attached to the vecotor elements using ```names(vec) = namevec```.
-#' 
-#' If we want to access a single element or a subset form a vecotr, we can work with indices. They are written in swquare brackets next to the vector name. For example ````myvector[4]``` returns the rth element of myvector and ```myvector[6] = 8``` changes the 6th element to take the value of 8. If the vector elements have names, we can also use those as indices like in ```myvector["elementname"]```
-## ------------------------------------------------------------------------
-# Create a vector "avgs":
-avgs <- c(.366, .358, .356, .349, .346)
-
-# Create a string vector of names:
-players <- c("Cobb","Hornsby","Jackson","O'Doul","Delahanty")
-
-# Assign names to vector and display vector:
-names(avgs) <- players
-avgs
-
-# Indices by number:
-avgs[2]
-avgs[1:4]
-
-# Indices by name:
-avgs["Jackson"]
-
-# Logical indices:
-avgs[ avgs>=0.35 ]
+xf1 <- factor(x, labels = c("bad", "okay", "good"), ordered = TRUE)
+xf2 <- factor(x, labels = c("bad", "okay", "good"))
 
 #' 
+#' <span style="color:red">Task</span> <u>**Practice more **</u>
+## ---- eval=FALSE---------------------------------------------------------
+## # variable gender with 20 "male" entries and 30 "female" entries
+## gender <- c(rep("male", _ ), rep(_, 30)_  # COMPLETE MISSING PARTS
+## gender <- _    # CREATE gender as factor
+## 
+## 
+## # R now treats gender as a nominal variable
+## summary(gender)
+
+#' 
+#' 
 #' ***
+#' 
+#' 
 #' ## Matrices
-#' Matrices are important tools for econometric analyses (think back to the first Tut). R has a powerful matrix algebra system. Most often in applied econometrics, matrices will be generated from an exisiting data set. But you can also build the from scratch with ```matrix(vec, nrow=m)``` (takes the numbers storeend in vector vec and puts them into a matrix with m rows). 
-#' Other options incluede: ```rbind(r1,r2)``` and ```cbind(c1,c2)``` in binding several vectors (which obviously need to have the same length) by row or column.
+#' Matrices are important tools for data analysis. R has a powerful matrix algebra system. Most often, matrices will be generated from an existing data set. But you can
+#' also build the from scratch with ```matrix(vec, nrow=m)``` (takes the numbers stored in vector ```vec``` and puts them into a matrix with m rows). 
+#' Other options include: ```rbind(r1,r2)``` and ```cbind(c1,c2)``` in binding several vectors (which obviously need to have the same length) by row or column.
 #' 
-## ------------------------------------------------------------------------
-# Generating matrix A from one vector with all values:
-v <- c(2,-4,-1,5,7,0)
-A <- matrix(v,nrow=2) 
-A
+#' 
+#' <span style="color:red">Task</span> <u>**Complete the empty spaces!**</u>
+## ---- eval=FALSE---------------------------------------------------------
+## # GENERATE matrix A from one vector with two rows
+## v <- c("AG.5", "G2", "AG.5", "G1", "AG.5", "G3")
+## A <- matrix(v, _ = 2)
+## 
+## # GENERATE matrix A.row in binding vec1 and vec2 AND generate matrix A.col in binding column wise:
+## vec1 <- c("AG.5", "AG.5", "AG.5")
+## vec2 <- c("G2", "G1", "G3")
+## 
+## A.row <- _____(vec1, vec2)
+## A.col <- ______
+## 
+## # Giving names to rows and columns:
+## colnames(A.col) <- c("Kathrin", "Hans")
+## rownames(A.col) <- c("Monday", "Wednesday", "Friday")
+## A.col
 
-# Generating matrix A from two vectors corresponding to rows:
-row1 <- c(2,-1,7); row2 <- c(-4,5,0)
-A <- rbind(row1, row2)
-A
+#' 
+#' **Core rule** for indexing all two dimensional objects in R:  [row , column]
+#' 
+#' <span style="color:red">Task</span> <u>**Complete the empty spaces!**</u>
+## ---- eval=FALSE---------------------------------------------------------
+## # EXTRACT value of first column and 2nd row
+## A.col[_ , _]
+## 
+## # GIVE all meal choices from Hans:
+## A.col[ , ]
+## 
+## # WHAT does Kathrin have for lunch on Monday? call Kathrin and Monday by name
+## A.col[_ , _]
+## 
+## 
+## # WHAT does Kathrin have on Monday and Friday?
+## A.col[_ , c(_ , _)]
 
-# Generating matrix A from three vectors corresponding to columns:
-col1 <- c(2,-4); col2 <- c(-1,5); col3 <- c(7,0)
-A <- cbind(col1, col2, col3) 
-
-# Giving names to rows and columns:
-colnames(A) <- c("Alpha","Beta","Gamma")
-rownames(A) <- c("Aleph","Bet") 
-A
-
-# Indexing for extracting elements (still using A from above):
-A[2,1]
-A[,2]
-A[,c(1,3)]
-
-
-# Direct multiplication (not matrix multiplication but multiplying elements at same place)
-A <- matrix( c(2,-4,-1,5,7,0), nrow=2)
-B <- matrix( c(2,1,0,3,-1,5), nrow=2)
-A*B
-
-# Transpose:
-(C <- t(B) )
-
-# Matrix multiplication:
-(D <- A %*% C )
-
-# Inverse:
-solve(D)
-
+#' 
+#' 
+#' Some quick matrix algebra: 
+## ---- eval=FALSE---------------------------------------------------------
+## 
+## # Element wise multiplication (not matrix multiplication but multiplying elements at same place)
+## A <- matrix(c(2, -4, -1, 5, 7, 0), nrow = 2)
+## B <- matrix(c(2, 1, 0, 3, -1, 5), nrow = 2)
+## A * B
+## 
+## # Matrix multiplication:
+## (D <- A %*% C)
+## 
+## # Transpose:
+## (C <- t(B))
+## 
+## # Inverse:
+## solve(D)
+## 
 
 #' 
 #' ## Lists
 #' A list is a generic collection of objects. Unlike vectors, components can be of different types.
 ## ------------------------------------------------------------------------
 # Generate a list object:
-mylist <- list( A=seq(8,36,4), this="that", idm = diag(3))
+mylist <- list(A = seq(8, 36, 4),
+               product = "Fruechtekorb",
+               idm = diag(3))
 
-# Print whole list: 
+# Print whole list:
 mylist
 
 # Vector of names:
@@ -225,44 +288,65 @@ names(mylist)
 # Print component "A":
 mylist$A
 
+
 #' 
 #' ***
+#' 
 #' ## Data Frames
-#' A data frame is an object that collects several variables and can be thought of as a rectangular shape with the rows representing the observational units and the columns representing the variables. As such, it is similar to a matrix. For us, the most important difference to a matrix is that a data frame can contain variables of different types (like numerical, logical, string and factor), whereas matrices can only contain numerical values. 
+#' A data frame is an object that collects several variables and can be thought of as a rectangular shape with the rows representing the observational units and the columns
+#' representing the variables. As such, it is similar to a matrix. For us, the most important difference to a matrix is that a data frame can contain variables of different
+#' types (like numerical, logical, string and factor), whereas matrices can only contain numerical values. 
 #' 
-#' Unlike a matrix, the columns alwways contain names which represent the variables. We can define a data frame from scratch by using the command ```data.frame``` or ```as. data.frame```
-## ------------------------------------------------------------------------
-# Define one x vector for all:
-year     <- c(2008,2009,2010,2011,2012,2013)
-# Define a matrix of y values:
-product1<-c(0,3,6,9,7,8); product2<-c(1,2,3,5,9,6); product3<-c(2,4,4,2,3,2)
-sales_mat <- cbind(product1,product2,product3)
-rownames(sales_mat) <- year
-# The matrix looks like this:
-sales_mat
+#' Unlike a matrix, the columns always contain names which represent the variables. We can define a data frame from scratch by using the command ```data.frame``` or ```as. data.frame```
+#' 
+#' <span style="color:red">Task</span> <u>**Complete the empty spaces!**</u>
+## ---- eval=FALSE---------------------------------------------------------
+## # Define one x vector for all:
+## days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+## 
+## # When are Eugen, Julia, and Lena in the office?
+## Eugen <-
+##   c(0, 0, 1, 1, 0)
+## Julia <- c(1, 1, 0, 0, 1)
+## Lena <- c(0, 0, 0, 1, "k")
+## 
+## # CBIND Eugen, Julia and Lena
+## presence.mat <- _____
+## 
+## # ASSIGN the rownames
+## rownames(presence.mat) <- ___
+## 
+## # The matrix looks like this:
+## presence.mat
+## 
+## # Create a data frame and display it:
+## presence <- as.data.frame(presence.mat)
+## presence
 
-# Create a data frame and display it:
-sales <- as.data.frame(sales_mat)
-sales
-
-#' The outputs of matrix ```sales_mat``` and ```sales``` look exactly the same, but they behave differently. In RStudio, the difference can be seen in the Workspace window. ```sales``` is desceibed as *6 obs. of 3 variables*.
-#' ***
+#' 
+#' The outputs of matrix ```presence.mat``` and ```presence``` look exactly the same, but they behave differently. In RStudio, the difference can be seen in the Workspace window. ```presence``` is described as *5 obs. of 3 variables*. Again, every column is an individual variable. 
+#' 
 #' We can address a single variable var of a data frame df using the matrix-like syntax ```df[, "var"]``` or by stating ```df$var```. This can be used for extracting the values of a variable but also for creating new variables. Sometimes, it is convenient not to have to type the name of the data frame several times within a command. The function ```with(df, some expression using vars of df``` can help. 
-## ------------------------------------------------------------------------
-# Accessing a single variable:
-sales$product2
-
-# Generating a new  variable in the data frame:
-sales$totalv1 <- sales$product1 + sales$product2 + sales$product3 
-sales
-# The same but using "with":
-sales$totalv2 <- with(sales, product1+product2+product3)
-sales
+#' 
+#' <span style="color:red">Task</span> <u>**Complete the empty spaces!**</u>
+## ---- eval=FALSE---------------------------------------------------------
+## # Accessing a single variable:
+## presence$Eugen
+## 
+## # GENERATE a new variable total.presence which sums up the total of people who are present on each day:
+## presence$total.presence <- presence$Eugen + ______ + ______
+## presence
+## 
+## # The same but using "with":
+## presence$total.presence2 <- with(presence, Eugen+Julia+Lena)
+## 
 
 #' ***
-#' Sometimes, we do not want to work with a whole data set but only with a subset. This can be easily ahcieved with the command ````subset(df, criterion)```, where *criterion* is a logical expression which evaluetes to TRUE for the rows which are to be selected. 
-## ------------------------------------------------------------------------
-# Subset: all years in which sales of product 3 were >=3
-subset(sales, product3>=3)
+#' Sometimes, we do not want to work with a whole data set but only with a subset. This can be easily achieved with the command ````subset(df, criterion)```, where *criterion* is a logical expression which evaluetes to TRUE for the rows which are to be selected. 
+#' 
+#' <span style="color:red">Task</span> <u>**Complete the empty spaces!**</u>
+## ---- eval=FALSE---------------------------------------------------------
+## # Subset: all days in which Lena is present
+## subset(presence, __==___)
 
 #' 
