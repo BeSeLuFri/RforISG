b845dc35ac7670d9ad9b49b872d9c3813106714f
Benedikt Fritz <b.fritz@zeppelin-university.net>
2019-06-19 18:05:47 +0200

    Data Viz Eurostat Example + Map

--- docs/data/Create data files for upload to github.R
+++ docs/data/Create data files for upload to github.R
@@ -1,3 +1,5 @@
+
+# SOEP data ----
 #Daten als Objekt importieren
 econ <- readstata13::read.dta13(file="data/offline/econometrics.dta" ,
                    convert.factors=F,
@@ -24,3 +26,33 @@ haven::write_sav(econ2, "data/soep.sav")
 
 openxlsx::write.xlsx(econ2, "data/soep.xlsx")
 
+
+# Eurostat Data----
+
+eurost <- read_csv2(file = "data/offline/data_uptodate.csv")
+
+eurost <- eurost %>%
+  filter(!geo_code %in% c("BE1", "BE2", "BE3", "EA19", "EE", "EU15", "EU28", "EA12", "EU27_2019", "DE_TOT", "EEA30", "EEA31", "EEA28",
+                          "EA18", "EU",   "EU27", "EA17", "RS", "MK", "TR", "ME", "AZ", "BY", "EA", "EFTA", "FX", "AL", "BA", "XK", 
+                          "LI", "US", "AZ", "GE", "UA", "MD", "RU", "AD", "AM", "MC", "SM")) %>%
+  select(geo_code, time, unemp_youth_t, gdp_gr, inv_per_empl, population_total_t, unemp_workagepop_t,
+         emmigration_t, immigration_t)
+
+
+a <- data.frame(geo_code = c("AT", "BE", "BG", "CH","CY","CZ","DE","DK","EL","ES","FI",
+                             "FR","HR","HU","IE","IS","IT","LT","LU","LV","MT","NL",
+                             "NO","PL","PT","RO","SE","SI","SK","UK"),
+                location = c("Central", "West", "East", "Central", "South", "Central", "Central", "North", "South", "South", "North",
+                             "West", "East", "Central", "North", "North", "South", "North", "North", "North", "South", "West",
+                             "North", "Central", "South", "East", "North", "Central", "Central", "North"),
+                geo_name = c("Austria", "Belgium", "Bulgaria", "Switzerland", "Cyprus", "Czech Republic", "Germany", "Denmark", "Greece","Spain", "Finland", 
+                             "France", "Croatia", "Hungary", "Ireland", "Iceland", "Italy", "Lithuania","Luxembourg", "Latvia", "Malta", "Netherlands",
+                             "Norway", "Poland", "Portugal", "Romania", "Sweden", "Slovenia", "Slovakia", "UK"))
+
+
+
+ 
+
+eurost <- left_join(x=eurost, y=a, by="geo_code")
+
+write_csv2(eurost, "data/eurostat_data.csv")
